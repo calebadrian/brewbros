6eac28652b975d749eaf42ee4ad22b8b6dd09d0e
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-13 16:33:30 -0600

    mods to login and register form

--- public/src/store/index.js
+++ public/src/store/index.js
@@ -47,6 +47,7 @@ export default new vuex.Store({
 
         //user and login actions
         createUser({ commit, dispatch }, payload) {
+            console.log(payload)
             auth.post('register', payload).then(res => {
                 commit('updateUser', res.data.user)
                 router.push({ name: 'Home' })
