dc33bdea799a3e25304c7d0e77271cac10d41f53
Trevor <trevor.spielman@gmail.com>
2018-03-13 10:46:10 -0600

    vue components created.

---
+++ public/src/components/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Profile">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
