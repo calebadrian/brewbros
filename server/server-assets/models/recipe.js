df42edf2035144351b845af185a9651060e19db3
Bryce Murray <brycepmurray711@gmail.com>
2018-03-13 10:10:55 -0600

    yeast and adjunct to go

---
+++ server/server-assets/models/recipe.js
@@ -0,0 +1,23 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let ObjectId = mongoose.SchemaTypes.ObjectId
+let schemaName = "Recipe"
+
+let schema = new Schema({
+    name: { type: String, required: true },
+    batchSize: { type: Number, required: true },
+    public: { type: boolean, default: true },
+    style: { type: String, required: true },
+    subStyle: { type: String },
+    boilTime: { type: Number, required: true },
+    personalComments: { type: String },
+    creatorId: { type: ObjectId, ref: 'User', required: true },
+    fermentables: [{}],
+    hops: [{}],
+    yeasts: [{}],
+    adjuncts: [{}],
+    created: { type: Number, required: true, default: Date.now() },
+})
+
+
+module.exports = mongoose.model(schemaName, schema)
