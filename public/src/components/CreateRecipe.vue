5d268be7f3aa34115be0263e80ea790f0dd73d03
Trevor <trevor.spielman@gmail.com>
2018-03-13 14:33:56 -0600

    added category dropdwon selector

--- public/src/components/CreateRecipe.vue
+++ public/src/components/CreateRecipe.vue
@@ -1,61 +1,63 @@
 <template>
   <div class="CreateRecipe">
-   <div class="container-fluid">
-     <form type="submit">
-     <div class="row">
-       <div class="col-sm-4">
-         <div class="form-group">
-           <input type="text" class="form-control" id="name" placeholder="recipe name">
+    <div class="container-fluid">
+      <form type="submit">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="form-group">
+              <input type="text" class="form-control" id="name" placeholder="recipe name">
+            </div>
           </div>
         </div>
-       </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="form-group">
-            <label for="batchSize">Batch Size</label>
-            <input type="number" class="form-control" id="batchSize" placeholder="#">
-            <p>gallons</p>
-            <input class="form-check-input" type="checkbox" id="private"> 
-            <label for="private">Private</label>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="form-group">
+              <label for="batchSize">Batch Size</label>
+              <input type="number" class="form-control" id="batchSize" placeholder="#">
+              <p>gallons</p>
+              <input class="form-check-input" type="checkbox" id="private">
+              <label for="private">Private</label>
+            </div>
           </div>
-        </div>
           <div class="col-sm-6">
             <div class="form-group">
               <select class="form-control" id="style" placeholder="style">
-                    <option v-for="style in styles">{{style.name}}</option>
+                <option v-for="style in styles">{{style.name}}</option>
+              </select>
+              <select class="form-control" id="subStyle" placeholder="Sub-Style">
+                <option v-for="category in categories">{{category.name}}</option>
               </select>
             </div>
           </div>
-      </div>
-    </form>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: 'CreateRecipe',
-        mounted() {
-            this.$store.dispatch('getStyles')
-        },
-        data() {
-            return {
+  export default {
+    name: 'CreateRecipe',
+    mounted() {
+      this.$store.dispatch('getStyles')
+    },
+    data() {
+      return {
 
-            }
-        },
-        methods: {
+      }
+    },
+    methods: {
 
-        },
+    },
 
-        computed: {
-            styles() {
-                return this.$store.state.styles
-            }
-        }
+    computed: {
+      styles() {
+        return this.$store.state.styles
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
