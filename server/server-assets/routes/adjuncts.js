878914ca6f94468fd88fc49183605babbe5482d5
Trevor <trevor.spielman@gmail.com>
2018-03-13 10:21:50 -0600

    fixed typo in Adjuncts Route. No longer broken

--- server/server-assets/routes/adjuncts.js
+++ server/server-assets/routes/adjuncts.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 var Recipe = require('../models/Recipe');
-var Adjucnts = require('../models/Adjunct');
+var Adjuncts = require('../models/Adjunct');
 
 
 
