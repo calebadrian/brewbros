7084656711d63c34c547720f04739eb281fe0c9e
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-12 22:17:58 -0600

    first commit

---
+++ public/src/store/index.js
@@ -0,0 +1,39 @@
+import vue from 'vue'
+import vuex from 'vuex'
+import axios from 'axios'
+import router from '../router'
+
+var production = !window.location.host.includes('localhost')
+var key = '?key=e96ab9f00ea6c4d6e6ad50967fc0627d&format=json'
+var baseUrl = production ? '//brewbook.herokuapp.com/' : '//localhost:3000/'
+
+let beerDB = axios.create({
+    baseURL: 'http://api.brewerydb.com/v2/',
+    timeout: 10000
+})
+
+var ourDB = axios.create({
+    baseURL: baseUrl + 'api/',
+    timeout: 5000,
+    withCredentials: true
+})
+
+var auth = axios.create({
+    baseURL: baseUrl + 'auth/',
+    timeout: 5000,
+    withCredentials: true
+})
+
+vue.use(vuex)
+
+export default new vuex.Store({
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+        
+    }
+})
---
+++ server/server-assets/db/mlab-config.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var connectionString = "mongodb://test:test@ds012538.mlab.com:12538/brewbros"
+
+var connection = mongoose.connection;
+mongoose.connect(connectionString);
+
+connection.on("error", err => {
+  console.error("mlab Error: ", err);
+});
+
+connection.once("open", () => {
+  console.log("connected to database");
+});
