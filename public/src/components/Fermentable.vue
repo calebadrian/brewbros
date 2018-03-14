0a9a4c218a60994f9dd6f40515aa4c0ec8755067
Bryce Murray <brycepmurray711@gmail.com>
2018-03-14 09:59:54 -0600

    same layout for fermentable and Steeping Grain as others using card

---
+++ public/src/components/Fermentable.vue
@@ -0,0 +1,36 @@
+<template>
+        <div class="steepingGrains">
+                <div class="card">
+                  <div class="card-header">
+                    <h5 class="card-title">Fermentables</h5>
+                  </div>
+                  <div class="card-body">
+                    <div class="form-group">
+                        <label for="fermentableAmount">lbs</label>
+                        <input type="number" class="form-control" id="fermentableAmount" placeholder="#">
+                      <label for="fermentable">Fermentable</label>
+                      <select type="text" class="form-control" id="fermentable"></select>
+                        <option v-for="fermentable in fermentables">{{fermentable.name}}</option>
+                    </div>
+                  </div>
+                </div>
+              </div>
+</template>
+
+<script>
+    export default {
+        name: 'Fermentable',
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ public/src/components/SteepingGrain.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="steepingGrains">
+    <div class="card">
+      <div class="card-header">
+        <h5 class="card-title">Steeping Grains</h5>
+      </div>
+      <div class="card-body">
+        <div class="form-group">
+            <label for="steepingGrainAmount">lbs</label>
+            <input type="number" class="form-control" id="steepingGrainAmount" placeholder="#">
+          <label for="steepingGrain">Fermentable To Steep</label>
+          <select type="text" class="form-control" id="steepingGrain"></select>
+            <option v-for="fermentable in fermentables">{{fermentable.name}}</option>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'SteepingGrains',
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
