0c148c3a1d1e3673683e391bbac57c7188f7226f
Trevor <trevor.spielman@gmail.com>
2018-03-14 11:03:30 -0600

    yeast  component created and added to createRecipe

--- public/src/components/CreateRecipe.vue
+++ public/src/components/CreateRecipe.vue
@@ -16,7 +16,7 @@
               <label for="batchSize">Batch Size</label>
               <input type="number" class="form-control" id="batchSize" placeholder="#">
               <p>gallons</p>
-                    <div class="private-box">
+              <div class="private-box">
                 <input class="form-check-input" type="checkbox" id="private">
                 <label for="private">Private</label>
               </div>
@@ -28,7 +28,7 @@
                 <option v-for="style in styles">{{style.name}}</option>
               </select>
               <select class="form-control" id="subStyle" placeholder="Sub-Style">
-                <option value="">Sub-Style</option>                              
+                <option value="">Sub-Style</option>
                 <option v-for="category in categories">{{category.name}}</option>
               </select>
               <label for="boilTime">Boil Time</label>
@@ -45,75 +45,84 @@
             </p>
           </div>
           <div class="col-sm-2">
-              <h4>Final Gravity</h4>
-              <p>
-                7.34
-              </p>
+            <h4>Final Gravity</h4>
+            <p>
+              7.34
+            </p>
           </div>
           <div class="col-sm-2">
-              <h4>Alcohol Percent</h4>
-              <p>
-                12%
-              </p>
+            <h4>Alcohol Percent</h4>
+            <p>
+              12%
+            </p>
           </div>
           <div class="col-sm-2">
-              <h4>IBU</h4>
-              <p>
-                4.14
-              </p>
+            <h4>IBU</h4>
+            <p>
+              4.14
+            </p>
           </div>
           <div class="col-sm-2">
-              <h4>Color</h4>
-              <p>
-                hex code!
-              </p>
+            <h4>Color</h4>
+            <p>
+              hex code!
+            </p>
           </div>
         </div>
         <div class="row justify-content-around">
           <div class="col-sm-4">
             <h4>Fermentables</h4>
-              <label for="fermentableAmount">lbs</label>
-              <input type="number" class="form-control" id="fermentableAmount" placeholder="#">
-              <p>lbs</p>
-              <select class="form-control" id="fermentable" placeholder="Fermenatable">
-                  <option value="">Ingredient</option>
-                  <option v-for="fermentable in fermentables">{{fermentable.name}}</option>
-                </select>
-                <i class="far fa-plus-square fa-2x" @click="addFermentable"></i>
+            <label for="fermentableAmount">lbs</label>
+            <input type="number" class="form-control" id="fermentableAmount" placeholder="#">
+            <p>lbs</p>
+            <select class="form-control" id="fermentable" placeholder="Fermenatable">
+              <option value="">Ingredient</option>
+              <option v-for="fermentable in fermentables">{{fermentable.name}}</option>
+            </select>
+            <i class="far fa-plus-square fa-2x" @click="addFermentable"></i>
           </div>
           <div class="col-sm-4">
-              <div class="card">
-                  <div class="card-header">
-                      <h5 class="card-title">Hops</h5>
-                  </div>
-                  <div class="card-body" id="hopForms">
-                    <hop></hop>
-                  </div>
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title">Hops</h5>
               </div>
+              <div class="card-body" id="hopForms">
+                <hop></hop>
+              </div>
+            </div>
           </div>
         </div>
         <div class="row justify-content-around">
-            <div class="col-sm-4">
-              <h4>Steeping Grains</h4>
-                <label for="steepingGrainsAmount">lbs</label>
-                <input type="number" class="form-control" id="steepingGrainsAmount" placeholder="#">
-                <p>lbs</p>
-                <select class="form-control" id="steepingGrains" placeholder="steepingGrains">
-                    <option value="">Steeping Grains</option>
-                    <option v-for="steepingGrain in steepingGrains">{{steepingGrain.name}}</option>
-                  </select>
-                  <i class="far fa-plus-square fa-2x" @click="addSteepingGrain"></i>
-            </div>
-            <div class="col-sm-4">
-              <adjunct></adjunct>
+          <div class="col-sm-4">
+            <h4>Steeping Grains</h4>
+            <label for="steepingGrainsAmount">lbs</label>
+            <input type="number" class="form-control" id="steepingGrainsAmount" placeholder="#">
+            <p>lbs</p>
+            <select class="form-control" id="steepingGrains" placeholder="steepingGrains">
+              <option value="">Steeping Grains</option>
+              <option v-for="steepingGrain in steepingGrains">{{steepingGrain.name}}</option>
+            </select>
+            <i class="far fa-plus-square fa-2x" @click="addSteepingGrain"></i>
+          </div>
+          <div class="col-sm-4">
+            <adjunct></adjunct>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title">Yeasts</h5>
+              </div>
+              <div class="card-body" id="yeastForms">
+                <yeast></yeast>
+              </div>
             </div>
           </div>
-          <div class="row">
-              <div class="col-sm-4"></div>
-              <div class="col-sm-4"></div>
-            </div>
-      </form>
+        <div class="col-sm-4"></div>
     </div>
+    </form>
+  </div>
   </div>
 </template>
 
@@ -121,47 +130,49 @@
   import navbar from './Navbar'
   import hop from './Hop'
   import adjunct from './Adjunct'
-    export default {
-        name: 'CreateRecipe',
-        mounted() {
-            this.$store.dispatch('getStyles')
-        },
-        data() {
-            return {
+  import yeast from './yeast'
+  export default {
+    name: 'CreateRecipe',
+    mounted() {
+      this.$store.dispatch('getStyles')
+    },
+    data() {
+      return {
 
-            }
-        },
-        methods: {
-          addHop(){
-            
-          }
-        },
+      }
+    },
+    methods: {
+      addHop() {
 
-        computed: {
-            styles() {
-                return this.$store.state.styles
-            }
-        },
-        components: {
-          navbar,
-          hop,
-          adjunct,
-        }
+      }
+    },
+
+    computed: {
+      styles() {
+        return this.$store.state.styles
+      }
+    },
+    components: {
+      navbar,
+      hop,
+      adjunct,
+      yeast,
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .stats {
-        justify-content: space-between
-    }
-    
-    .private-box {
-        padding-left: 2rem
-    }
-    
-    .row {
-        padding-left: 0px;
-        padding-right: 0px
-    }
+  .stats {
+    justify-content: space-between
+  }
+
+  .private-box {
+    padding-left: 2rem
+  }
+
+  .row {
+    padding-left: 0px;
+    padding-right: 0px
+  }
 </style>
