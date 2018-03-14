69e640be98d7d87d232c21f0cb547c1d5cb6639f
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-14 10:31:31 -0600

    Adjunct component for form added

---
+++ public/src/components/Adjunct.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="adjunct">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Other Ingredients</h5>
+            </div>
+            <div class="card-body">
+                <form @submit.prevent="addAdjunct">
+                    <div class="form-group">
+                        <h6>Amount: </h6>
+                        <input type="number" name="adjunctAmount">
+                        <label for="adjunctAmount">oz</label>
+                    </div>
+                    <div class="form-group">
+                        <h6>Ingredient: </h6>
+                        <select class="form-control" id="adjunct" placeholder="Adjunct">
+                            <option v-for="adjunct in adjuncts">{{adjunct.name}}</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <h6>Boil Time: </h6>
+                        <input type="number" name="boilTime">
+                        <label for="boilTime">min</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.$store.dispatch('getAdjuncts')
+        },
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+            adjuncts() {
+                return this.$store.state.adjuncts
+            }
+        },
+    }
+</script>
+
+<style scoped>
+</style>
