03fcadcc05c6b546fd548b254a072a1f31b72882
Bryce Murray <brycepmurray711@gmail.com>
2018-03-14 11:23:56 -0600

    oops

--- public/src/components/SteepingGrain.vue
+++ public/src/components/SteepingGrain.vue
@@ -24,7 +24,7 @@
             }
         },
         computed: {
-            hops() {
+            fermentables() {
                 return this.$store.state.fermentables
             }
         },
