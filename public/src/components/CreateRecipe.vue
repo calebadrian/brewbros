7ee0b3e382691cf8f79f232bd67c1b875e804919
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-16 12:02:09 -0600

    changed on click to a question mark

--- public/src/components/CreateRecipe.vue
+++ public/src/components/CreateRecipe.vue
@@ -80,8 +80,9 @@
           <div class="col-sm-2">
             <h4>Matches Style</h4>
             <div v-if="recipe.style != ''">
-              <i class="fas fa-2x fa-check-circle" v-if="stats.styleCorrect" @click="styleDataToggle = !styleDataToggle"></i>
-              <i class="fas fa-2x fa-times-circle" @click="styleDataToggle = !styleDataToggle" v-else></i>
+              <i class="fas fa-2x fa-check-circle" v-if="!stats.styleCorrect"></i>
+              <i class="fas fa-2x fa-times-circle" v-else></i>
+              <i class="fas fa-question-circle" @click="styleDataToggle = !styleDataToggle"></i>
             </div>
           </div>
         </div>
