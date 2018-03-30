82dd77a7123a9fa473aa005f5a933a65e5634d00
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-30 12:47:08 -0600

    Browsing recipes into tabs

--- public/src/components/BrowseRecipes.vue
+++ public/src/components/BrowseRecipes.vue
@@ -6,33 +6,55 @@
         <div class="col-sm-12">
           <v-select label="name" value=" " v-model="style" :options="styles" class="selectFormat" placeholder="Sort by Style"></v-select>
         </div>
-        <div class="col-md-3 col-sm-12" v-if="!recipe.private && !style || style == null || recipe.style == style.name" v-for="recipe in allRecipes">
-          <recipe :recipe="recipe"></recipe>
-          <div class="card-footer text-muted">
-            <button type="button" class="btn btn-primary" data-toggle="modal" :recipe='recipe' :data-target="'#'+recipe._id">
-              View Full Recipe
-            </button>
-            <button type="button" class="btn btn-success" :recipe='recipe' v-if="recipe.creatorId != user._id && !recipe.favorited.includes(user._id) && user.name"
-              @click="favorite({user: user, recipe: recipe})">
-              Add to Favorites
-            </button>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">All Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Followers Recipes</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3 col-sm-12" v-if="!recipe.private && !style || style == null || recipe.style == style.name" v-for="recipe in allRecipes">
+              <recipe :recipe="recipe"></recipe>
+              <div class="card-footer text-muted">
+                <button type="button" class="btn btn-primary" data-toggle="modal" :recipe='recipe' :data-target="'#'+recipe._id">
+                  View Full Recipe
+                </button>
+                <button type="button" class="btn btn-success" :recipe='recipe' v-if="recipe.creatorId != user._id && !recipe.favorited.includes(user._id) && user.name"
+                  @click="favorite({user: user, recipe: recipe})">
+                  Add to Favorites
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <h3>My Followers Recipes:</h3>
-        </div>
-        <div class="col-md-3 col-sm-12" v-if="!recipe.private && !style || style == null || recipe.style == style.name" v-for="recipe in myFollowersRecipes">
-          <recipe :recipe="recipe"></recipe>
-          <div class="card-footer text-muted">
-            <button type="button" class="btn btn-primary" data-toggle="modal" :recipe='recipe' :data-target="'#'+recipe._id">
-              View Full Recipe
-            </button>
-            <button type="button" class="btn btn-success" :recipe='recipe' v-if="recipe.creatorId != user._id && !recipe.favorited.includes(user._id) && user.name"
-              @click="favorite({user: user, recipe: recipe})">
-              Add to Favorites
-            </button>
+      <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-12">
+              <h3>My Followers Recipes:</h3>
+            </div>
+            <div class="col-md-3 col-sm-12" v-if="!recipe.private && !style || style == null || recipe.style == style.name" v-for="recipe in myFollowersRecipes">
+              <recipe :recipe="recipe"></recipe>
+              <div class="card-footer text-muted">
+                <button type="button" class="btn btn-primary" data-toggle="modal" :recipe='recipe' :data-target="'#'+recipe._id">
+                  View Full Recipe
+                </button>
+                <button type="button" class="btn btn-success" :recipe='recipe' v-if="recipe.creatorId != user._id && !recipe.favorited.includes(user._id) && user.name"
+                  @click="favorite({user: user, recipe: recipe})">
+                  Add to Favorites
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
